01831292821bc674a0f057bc50d31083a745cb04
ZimorodekPl <zimorodek.pl@gmail.com>
2022-05-16 17:05:23 +0200

    fixed smtp sender connection error

--- src/lib/mailUtils.js
+++ src/lib/mailUtils.js
@@ -9,13 +9,16 @@ module.exports = {
 	async sendMail(){
 	console.log('Siema1');
 	let transporter = nodemailer.createTransport({
-		host: "smtp.sendgrid.net",
-		port: 587,
+		host: "127.0.0.1",
+		port: 25,	
 		secure: false, // true for 465, false for other ports
 		auth: {
 		user: "apikey", // generated ethereal user
 		pass: "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", // generated ethereal password
 		},
+		tls: {
+			rejectUnauthorized: false
+		}
 	});
 	console.log('Siema2');
 	const info = await transporter.sendMail({
@@ -24,7 +27,7 @@ module.exports = {
 		subject: "Hello ✔", // Subject line
 		text: "Hello world?", // plain text body
 		html: "<b>Hello world?</b>", // html body
-	});
+	}).catch((error) => {console.error(error);});
 	console.log('Siema3');
 	return info.messageId;
 }};
