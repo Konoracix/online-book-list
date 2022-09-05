c92e615da563d95d5c1c72f5e3c406d45696d118
ZimorodekPl <zimorodek.pl@gmail.com>
2022-09-05 17:34:23 +0200

    added bcrypt password encryption

--- db/seeds/03_user.js
+++ db/seeds/03_user.js
@@ -5,19 +5,16 @@ const bcrypt = require('bcrypt');
  * @returns { Promise<void> } 
  */
 
-function getHashPassword(err, hash){
-	console.log(hash)
-}
-
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
+  var salt = bcrypt.genSaltSync(12);
   await knex(tableNames.location).del()
   await knex(tableNames.userList).del()
   await knex(tableNames.location).insert([
     {country: 'Poland', postcode: '44-122', city: 'Katowice', street: 'Zimorodka', street_number: '5', house_number: '2'}
   ]);
 	await knex(tableNames.userList).insert([
-		{name: 'Robert', surname: 'Makłowicz', address_id: 1, mail: 'radek.sdasda@poczta.fm', phone_number: 123456789, password: 'asdasd'}
+		{name: 'Robert', surname: 'Makłowicz', address_id: 1, mail: 'radek.sdasda@poczta.fm', phone_number: 123456789, password: bcrypt.hashSync('myPlaintextPassword', salt)}
 	]);
-	bcrypt.hash('myPlaintextPassword', 12, getHashPassword(err, hash));
+  console.log(bcrypt.hashSync('myPlaintextPassword', "$2b$12$azy2qqDBi5u5p9fzVc0YeOFwK27gKpdi5uJsG00fhIN0t/n"))
 };
